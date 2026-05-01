2fa3fcd55dad2ffb7f2d71987a7ca633f13cecad
Lowie Dave Dichoson <lowiedave30@gmail.com>
2026-05-01 22:13:54 +0800

    chore: find largest num solution

---
+++ coding-problems/arrays/find-largest-num.js
@@ -0,0 +1,13 @@
+function findLargest(numbers) {
+    let largest = 0;
+
+    for (let num of numbers) {
+        if (num > largest) {
+            largest = num;
+        }
+    }
+
+    return largest;
+}
+
+console.log(findLargest([1, 2, 3, 4, 5, 6, 25, 5, 4, 3, 2]));
